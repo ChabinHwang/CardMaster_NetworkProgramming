2350938d4bc6344d949ea7865e6634149f929d13
chabin37 <7chabin@naver.com>
2024-11-23 17:32:06 +0900

    login부분 구현 완료

---
+++ CardMaster/src/main_signup_login/Main.java
@@ -0,0 +1,9 @@
+package main_signup_login;
+
+public class Main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new HomeScreen();
+        });
+    }
+}
