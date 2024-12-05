97c3e1b1bfe4ffa7fe675b46e1e509850d421268
chabin37 <7chabin@naver.com>
2024-12-06 01:45:50 +0900

    세팅초기화

--- CardMaster/src/cardmaster/main_signup_login/Main.java
+++
@@ -1,9 +0,0 @@
-package cardmaster.main_signup_login;
-
-public class Main {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            new HomeScreen();
-        });
-    }
-}
---
+++ app/src/main/java/src/cardmaster/main_signup_login/UserDatabase.java
@@ -0,0 +1,20 @@
+package src.cardmaster.main_signup_login;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserDatabase {
+    private static final Map<String, String> users = new HashMap<>();
+
+    public static boolean addUser(String id, String password) {
+        if (users.containsKey(id)) {
+            return false;
+        }
+        users.put(id, password);
+        return true;
+    }
+
+    public static boolean validateUser(String id, String password) {
+        return users.containsKey(id) && users.get(id).equals(password);
+    }
+}
