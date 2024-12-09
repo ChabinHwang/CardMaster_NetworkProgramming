0bfba438a3eb61e2b8100686c17e832db4ce0d33
eunki lee <dmsrl4136@naver.com>
2024-12-09 17:50:17 +0900

    add quit

---
+++ app/src/main/java/src/cardmaster/main_signup_login/handler/BtBaccaratQuitHandler.java
@@ -0,0 +1,18 @@
+package src.cardmaster.main_signup_login.handler;
+
+import src.cardmaster.main_signup_login.ClientUI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BtBaccaratQuitHandler implements ActionListener {
+    ClientUI cu;
+    public BtBaccaratQuitHandler(ClientUI cu){
+        this.cu = cu;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+        cu.net.sendQuitRequest();
+    }
+}
---
+++ app/src/main/java/src/cardmaster/main_signup_login/handler/BtBlackJackQuitHandler.java
@@ -0,0 +1,17 @@
+package src.cardmaster.main_signup_login.handler;
+
+import src.cardmaster.main_signup_login.ClientUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BtBlackJackQuitHandler implements ActionListener {
+    ClientUI cu;
+    public BtBlackJackQuitHandler(ClientUI cu){
+        this.cu = cu;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+        cu.net.sendQuitRequest();
+    }
+}
